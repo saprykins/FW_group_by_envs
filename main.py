ba6086797603c3949fc716d3f051a5dc3beed31f
saprykins <60004824+saprykins@users.noreply.github.com>
2024-09-17 10:15:47 +0200

    Update main.py
    export to csv instead of excel

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pandas as pd
 import os
 from datetime import datetime
 
+
 # Read the source excel file into a pandas DataFrame 
 df = pd.read_excel('source_file.xlsx') 
+
 
 # Convert date columns to datetime objects
 df['Planned Cutover date'] = pd.to_datetime(df['Planned Cutover date'], format='%d-%m-%Y', errors='coerce')
 df['Actual Cutover date'] = pd.to_datetime(df['Actual Cutover date'], format='%d-%m-%Y', errors='coerce')
+
 
 # Filter the DataFrame based on the specified conditions
 filtered_df = df[(df['Planned Cutover date'] > datetime.today()) & 
@@ -16,13 +19,17 @@ filtered_df = df[(df['Planned Cutover date'] > datetime.today()) &
                  (df['Entity'] == 'AXA UK & Ireland')]
                  # (df['App State'] != '2.2/ Migration On Going')]
 
+
+mydir = 'C:/Users/B****/Downloads/coding/servers_split_by_envs/results' 
+
+
 # Group the DataFrame by the 'Environment' column 
 grouped = filtered_df.groupby('Environment') 
-mydir = 'C:/Users/B449OH/Downloads/coding/servers_split_by_envs/results' 
+
 
 # Iterate through the groups and create separate excel files for each environment 
 for environment, group in grouped: 
     environment = environment.replace('/', ' - ').replace('?', ' - ').replace(':', ' - ').replace('\\', ' - ')
-    output_file_name = environment + '.xlsx' 
+    output_file_name = environment + '.csv' 
     output_path = os.path.join(mydir,output_file_name) 
-    group[['Environment','Source server']].to_excel(output_path, index=False)
+    group[['Environment','Source server']].to_csv(output_path, index=False)
